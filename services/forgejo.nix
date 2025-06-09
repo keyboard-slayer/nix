673d06d98874e7c94111e6390c2e01f859259908
keyboard-slayer <git@keyb.moe>
2025-06-09 23:41:42 +0200

    Forgejo: Fix SSH

--- services/forgejo.nix
+++ services/forgejo.nix
@@ -2,7 +2,10 @@
   services.forgejo = {
     enable = true;
     settings.server = {
+      START_SSH_SERVER = true;
+      USE_COMPACT_SSH_URI = false;
       SSH_PORT = 1337;
+      SSH_DOMAIN = "192.168.0.69";
       HTTP_PORT = 3030;
       ROOT_URL = "https://git.keyb.moe/";
       APP_NAME = "(つ▀¯▀)つ";
@@ -11,5 +14,6 @@
 
   networking.firewall.allowedTCPPorts = [
     3030
+    1337
   ];
 }
