238dd57d920408b0691dd4f6e4cdf537a18e8448
keyboard-slayer <git@keyb.moe>
2025-06-10 00:07:23 +0200

    fix missconfig forgejo

--- services/forgejo.nix
+++ services/forgejo.nix
@@ -1,14 +1,19 @@
 {
   services.forgejo = {
     enable = true;
-    settings.server = {
-      START_SSH_SERVER = true;
-      USE_COMPACT_SSH_URI = false;
-      SSH_PORT = 1337;
-      SSH_DOMAIN = "192.168.0.69";
-      HTTP_PORT = 3030;
-      ROOT_URL = "https://git.keyb.moe/";
-      APP_NAME = "(つ▀¯▀)つ";
+    settings = {
+      server = {
+        SSH_PORT = 1337;
+        SSH_DOMAIN = "192.168.0.69";
+        HTTP_PORT = 3030;
+        ROOT_URL = "https://git.keyb.moe/";
+      };
+      service = {
+        APP_NAME = "(つ▀¯▀)つ";
+        START_SSH_SERVER = true;
+        DISABLE_REGISTRATION = true;
+        USE_COMPACT_SSH_URI = false;
+      };
     };
   };
 
